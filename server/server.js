69bd45751f3ad93143fd545ff56fe932ad03ca4e
Mwaisammy <mwaisamuel432@gmail.com>
2025-02-22 13:00:28 +0300

    restful api

--- server/server.js
+++ server/server.js
@@ -3,23 +3,115 @@
 import express from "express"
 import dotenv from "dotenv"
 import mongoose from "mongoose";
+import Chat from "./chatModels/chatModels.js";
+
 dotenv.config();
+
+
 
 
 //instatiate the express app
 const app = express()
+
+//declare a middleware
+app.use(express.json())
+
 const port  = process.env.PORT || 5000
 const MONGOURI = process.env.MONGOURI
 
 
+
+
 //api routes
 
+//get chat messages from the db
+app.get("/", async(req, res) => {
+try {
 
-app.get("/", (req, res) => {
-    res.send("Hello world")
+
+    const chats = await Chat.find({}).sort({ createdAt: -1});
+
+
+    return res.status(200).json({
+        count: chats.length,
+        data: chats
+    })
+    
+    
+} catch (error) {
+
+    return res.status(500).json({
+        message: "Error getting chats"
+    })
+    
+}
+
 })
 
 
+//sending chat messages to the db
+app.post("/", async (req, res) => {
+
+    try {
+
+        const { name, message } = req.body
+
+        if( !name || !message){
+            return res.status(400).json({
+                message: "Please enter all fields"
+            
+            })
+        }
+        
+
+            const newChat = {name, message}
+
+            console.log(newChat)
+
+            const chat = await Chat.create(newChat)
+
+
+            // res.status(200).json({
+            //     message: "Chat posted successfully"
+            // })
+
+            return res.status(201).json(chat);
+        
+    } catch (error) {
+
+        return res.status(500).json({
+            message: "Error creating chats"
+        })
+        
+    }
+
+
+})
+
+//updating chat messages to the db
+app.put("/", (req, res) => {
+
+    try {
+        res.status(200).json({
+            message:"Updating chats"
+        })
+        
+    } catch (error) {
+        return res.status(500).json({
+            message:"Error updating chats"
+        })
+        
+    }
+   
+})
+
+
+//deleting chat messages from the db
+app.delete("/", (req, res) => {
+    return res.status(200).json({
+            message:"Deleting chats"
+        })
+})
 
 mongoose.connect(MONGOURI)
 
